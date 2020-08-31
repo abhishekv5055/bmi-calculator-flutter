85624af439c2f945afc37c2ee4ea4b62378984e2
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-08-31 13:55:27 +0530

    constant file complete

---
+++ lib/constant.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kBottomContainerColor = Color(0xFFE83D66);
+const kActiveCardColor = Color(0xFF323244);
+const kInactiveCardColor = Color(0xFF111328);
+const kMinSliderValue = 120.0;
+const kMaxSliderValue = 220.0;
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8B8EA1),
+);
+
+const kHeightTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kResultTextStyle = TextStyle(
+  fontSize: 80.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: Colors.green,
+  fontWeight: FontWeight.w500,
+);
+
+const kResultTopTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontWeight: FontWeight.bold,
+);
